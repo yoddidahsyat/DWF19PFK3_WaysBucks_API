97a918916d5469b4ec34027e4a506b54ee60d910
yoddi dahsyat <yoddi.dahsyat@gmail.com>
2021-01-30 00:29:31 +0700

    deploy

--- models/user.js
+++ models/user.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
          */
         static associate(models) {
             // define association here
-            User.hasMany(models.Transaction, { as: "transactions" });
+            User.hasMany(models.Transaction, { as: "transactions", foreignKey: "userId" });
         }
     };
     User.init({
